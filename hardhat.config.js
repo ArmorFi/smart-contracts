789bc21145593b4675802cf85ae25458fcfa4f18
Dragos Horodnic <dragos@horodnic.com>
2021-08-06 12:56:21 +0300

    Use uint96 for cover amount, remove bitwise getters and setters

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('@nomiclabs/hardhat-web3');
 require('@nomiclabs/hardhat-truffle5');
 require('@nomiclabs/hardhat-etherscan');
-require("@nomiclabs/hardhat-ethers");
+require('@nomiclabs/hardhat-ethers');
 require('solidity-coverage');
 require('hardhat-contract-sizer');
 
@@ -63,9 +63,7 @@ for (const network of ['MAINNET', 'KOVAN']) {
   networks[network.toLowerCase()] = { accounts, gasPrice, gasLimit, url };
 }
 
-const compilerSettings = process.env.ENABLE_OPTIMIZER
-  ? { optimizer: { enabled: true, runs: 200 } }
-  : {};
+const compilerSettings = process.env.ENABLE_OPTIMIZER ? { optimizer: { enabled: true, runs: 200 } } : {};
 
 module.exports = {
   contractSizer: {
@@ -82,6 +80,7 @@ module.exports = {
     exit: true,
     bail: false,
     recursive: false,
+    timeout: 60 * 60 * 60, // 1 hour
   },
 
   networks,
